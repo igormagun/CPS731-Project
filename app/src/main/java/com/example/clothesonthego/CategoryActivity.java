115f987a647d9784c80ddc66c20945b4b3b000f4
nbmal <nabzoom@hotmail.com>
2020-11-08 12:25:31 -0500

    Remade category activities and set CategoryActivity.java as launcher activity

--- app/src/main/java/com/example/clothesonthego/CategoryActivity.java
+++ app/src/main/java/com/example/clothesonthego/CategoryActivity.java
@@ -2,21 +2,13 @@ package com.example.clothesonthego;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
-
 public class CategoryActivity extends AppCompatActivity {
-    CategoryListActivity category;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        category = new category(this);
-
-        // TODO: Complete the code
+        setContentView(R.layout.activity_category);
     }
 }
