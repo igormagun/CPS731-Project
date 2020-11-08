68b2bb5caf68468c7f0abbcd81703f62dfbdafa4
nbmal <nabzoom@hotmail.com>
2020-11-08 11:49:41 -0500

    Adding category activities

--- app/src/main/java/com/example/clothesonthego/CategoryActivity.java
+++ app/src/main/java/com/example/clothesonthego/CategoryActivity.java
@@ -2,13 +2,21 @@ package com.example.clothesonthego;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+public class CategoryActivity extends AppCompatActivity {
+    CategoryListActivity category;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        category = new category(this);
+
+        // TODO: Complete the code
     }
 }
