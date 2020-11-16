954cb7509093ad3c04d3c1a4b77524f7b9e15874
igormagun <igormagun@gmail.com>
2020-11-16 13:20:50 -0500

    Removed unused activity, added TODO

--- app/src/main/java/com/example/clothesonthego/CartActivity.java
+++ app/src/main/java/com/example/clothesonthego/CartActivity.java
@@ -24,5 +24,7 @@ public class CartActivity extends AppCompatActivity {
         // Note that destination returns null if no destination has been set by the user
         String destination = cart.getDestination();
         HashMap<String, Integer> products = cart.getProducts();
+
+        // TODO: Determine how best to display the data in the UI, implement accordingly
     }
 }
