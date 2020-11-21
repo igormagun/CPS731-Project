6153b15f6230ffa2ecad1affd2a630f58b1a9f7f
nbmal <nabzoom@hotmail.com>
2020-11-21 16:58:18 -0500

    Added logout button in toolbar

--- app/src/main/java/com/example/clothesonthego/toolbar.java
+++ app/src/main/java/com/example/clothesonthego/toolbar.java
@@ -1,6 +1,26 @@
 package com.example.clothesonthego;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Toolbar extends Activity {
+    Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar);
+
+        logout = findViewById(R.id.LogoutButton);
+
+        logout.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
+            startActivity(intent);
+        });
+    }
 }
