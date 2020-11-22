ce1af80de391044de9ee09118141ef5b35015bd0
igormagun <igormagun@gmail.com>
2020-11-22 15:59:41 -0500

    Removed non-functional menu for now, re-added constraints to login button

--- app/src/main/java/com/example/clothesonthego/Toolbar.java
+++ app/src/main/java/com/example/clothesonthego/Toolbar.java
@@ -17,24 +17,6 @@ public class Toolbar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toolbar);
 
-        // TODO: Implement as MenuItem to fix this button not working
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.logout_button) {
-            FirebaseAuth.getInstance().signOut();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        // TODO: Implement MenuItem for logout and cart
     }
 }
